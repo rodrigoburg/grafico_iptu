1c68dcbeb4d4ab616bccfad382e753872acbd6f6
rodrigoburg <rodrigo.burgarelli1@gmail.com>
2016-07-01 20:59:10 -0300

    texto funcionando!

--- js/modelo.js
+++ js/modelo.js
@@ -1,3 +1,12 @@
+String.prototype.capitalize = function() {
+    var saida = ""
+    this.split(" ").forEach(function (d) {
+        saida += d.charAt(0).toUpperCase() + d.slice(1).toLowerCase() + " ";
+    })
+    return saida
+}
+
+
 var width = $("body").width()* 0.9
 var height = 550
 var margins = {
@@ -7,57 +16,23 @@ var margins = {
     top:80
 }
 
+var distrito_selec = "TOTAL";
+var tipo_select = "TOTAL";
+var valor_select;
+
 var iniciar = function() {
-
-    $('#botao_distrito').dropdown();
-
     $.getJSON("dados/valores.json", function (d) {
         window.dados = d
         window.dados_parseados = parseia_dados(d);
         desenha_grafico(dados_parseados);
+        inicia_dropdowns()
     })
 }
 
-function conserta_faixa(faixa) {
-    traducao = {
-        '100000':'Menor que R$ 100 mil',
-        '200000':'Entre R$ 100 mil e R$ 200 mil',
-        '300000':'Entre R$ 200 mil e R$ 300 mil',
-        '500000':'Entre R$ 300 mil e R$ 500 mil',
-        '1000000':'Entre R$ 500 mil e R$ 1 milhão',
-        '10000000000':'Mais que R$ 1 milhão'
-    }
-    return traducao[faixa]
-}
-
-function conserta_tipo(tipo) {
-    if (tipo == 'terreno_vazio') return "TERRENO VAZIO"
-    if (tipo == 'total') return 'total'
-    return tipo.toUpperCase()
-
-}
-
-function parseia_dados(data) {
-    //nessa variável aqui vamos guardar os itens no formato do dimple (lista de dicionários)
-    var saida = []
-
-    //loopão
-    var distritos = []
-    var tipos = []
-    for (var distrito in data) {
-        if (distrito != 'total' && distrito != 'NA') distritos.push(distrito)
-        for (var tipo in data[distrito]) {
-            if ((tipos.indexOf(conserta_tipo(tipo)) == -1) && (tipo != 'total')) tipos.push(conserta_tipo(tipo))
-            for (var faixa in data[distrito][tipo]) {
-                var item = {"distrito":distrito,"tipo":conserta_tipo(tipo),"faixa":conserta_faixa(faixa),"numero":data[distrito][tipo][faixa]}
-                saida.push(item)
-            }
-        }
-    }
-
-    //agora povoa o dropdown de distritos
+function inicia_dropdowns() {
+    //povoa o dropdown de distritos
     distritos = distritos.sort()
-    $('#menu_distrito').append('<li><a href="#">TOTAL</a></li>')
+    $('#menu_distrito').append('<li><a href="#">TOTAL <span class="glyphicon glyphicon-ok distrito_ok"></span></a></li>')
     $('#menu_distrito').append(' <li role="separator" class="divider"></li>')
 
     distritos.forEach(function(distrito) {
@@ -67,7 +42,7 @@ function parseia_dados(data) {
 
     //agora povoa o dropdown de tipos
     tipos = tipos.sort()
-    $('#menu_tipo').append('<li><a href="#">TOTAL</a></li>')
+    $('#menu_tipo').append('<li><a href="#">TOTAL <span class="glyphicon glyphicon-ok tipo_ok"></span></a></li>')
     $('#menu_tipo').append(' <li role="separator" class="divider"></li>')
 
     tipos.forEach(function(tipos) {
@@ -75,6 +50,99 @@ function parseia_dados(data) {
         $('#menu_tipo').append(opcao)
     })
 
+    //agora iniciamos ambos e colocamos os eventos
+    $('#botao_distrito').dropdown();
+    $("#menu_distrito").find("li").on("click", function () {
+        $(".distrito_ok").remove()
+        var el = $(this)
+        distrito_selec = $(this).text().trim()
+        $(this).html('<a href="#">'+distrito_selec + ' <span class="glyphicon glyphicon-ok distrito_ok"></span></a>')
+        atualiza_grafico(dados_parseados,distrito_selec,tipo_select);
+    })
+
+    $('#botao_tipo').dropdown();
+    $("#menu_tipo").find("li").on("click", function () {
+        $(".tipo_ok").remove()
+        var el = $(this)
+        tipo_select = $(this).text().trim()
+        $(this).html('<a href="#">'+tipo_select + ' <span class="glyphicon glyphicon-ok tipo_ok"></span></a>')
+        atualiza_grafico(dados_parseados,distrito_selec,tipo_select);
+    })
+
+    //aqui coloca o evento no botão de calcular
+    $("#calcular").on("click",function () {
+        valor_select = $("#usr").val()
+        if (valor_select) {
+            atualiza_textos(valor_select);
+        }
+
+    })
+}
+
+
+function conserta_faixa(faixa) {
+    traducao = {
+        '100000':'Menor que R$ 100 mil',
+        '200000':'Entre R$ 100 mil e R$ 200 mil',
+        '300000':'Entre R$ 200 mil e R$ 300 mil',
+        '500000':'Entre R$ 300 mil e R$ 500 mil',
+        '1000000':'Entre R$ 500 mil e R$ 1 milhão',
+        '10000000000':'Mais que R$ 1 milhão',
+        'TOTAL':'TOTAL'
+    }
+    return traducao[faixa]
+}
+
+function acha_faixa(faixa) {
+    faixa = parseInt(faixa);
+    if (faixa < 100000) faixa = '100000'
+    else if (faixa < 200000) faixa = '200000'
+    else if (faixa < 300000) faixa = '300000'
+    else if (faixa < 500000) faixa = '500000'
+    else if (faixa < 1000000) faixa = '1000000'
+    else faixa = '10000000000'
+    return faixa
+}
+
+function conserta_tipo(tipo) {
+    if (tipo == 'terreno_vazio') return "TERRENO VAZIO"
+    return tipo.toUpperCase()
+}
+
+function traduz_tipo(tipo) {
+    tipo = tipo.replace(" ","_")
+    traducao = {
+        'residencial':'imóveis residenciais',
+        'comercial':'imóveis comerciais',
+        'terreno_vazio':'terrenos vazios',
+        'outros':'imóveis de uso variado',
+        'TOTAL':'imóveis'
+    }
+    return traducao[tipo]
+}
+
+function traduz_distrito(distrito) {
+    if (distrito == 'TOTAL') return "em São Paulo"
+    return "no distrito de " + distrito.capitalize()
+}
+
+function parseia_dados(data) {
+    //nessa variável aqui vamos guardar os itens no formato do dimple (lista de dicionários)
+    var saida = []
+
+    //loopão
+    window.distritos = []
+    window.tipos = []
+    for (var distrito in data) {
+        if (distrito != 'TOTAL' && distrito != 'NA') distritos.push(distrito)
+        for (var tipo in data[distrito]) {
+            if ((tipos.indexOf(conserta_tipo(tipo)) == -1) && (tipo != 'TOTAL')) tipos.push(conserta_tipo(tipo))
+            for (var faixa in data[distrito][tipo]) {
+                var item = {"distrito":distrito,"tipo":conserta_tipo(tipo),"faixa":conserta_faixa(faixa),"porcentual":parseInt((data[distrito][tipo][faixa]/data[distrito][tipo]["TOTAL"])*1000)/10}
+                saida.push(item)
+            }
+        }
+    }
     return saida
 }
 
@@ -97,8 +165,8 @@ function desenha_grafico(data) {
     ordem_x = ['Menor que R$ 100 mil','Entre R$ 100 mil e R$ 200 mil','Entre R$ 200 mil e R$ 300 mil','Entre R$ 300 mil e R$ 500 mil','Entre R$ 500 mil e R$ 1 milhão','Mais que R$ 1 milhão']
 
     var svg = dimple.newSvg("#grafico",width,height)
-    data = dimple.filterData(data, "distrito", "total");
-    data = dimple.filterData(data, "tipo", "total");
+    data = dimple.filterData(data, "distrito", distrito_selec);
+    data = dimple.filterData(data, "tipo", tipo_select);
     data = dimple.filterData(data, "faixa", ordem_x);
 
     var myChart = new dimple.chart(svg, data);
@@ -109,8 +177,9 @@ function desenha_grafico(data) {
     x.addOrderRule(ordem_x)
     //x.addGroupOrderRule(ordem_x)
 
-    var y = myChart.addMeasureAxis("y", "numero");
+    var y = myChart.addMeasureAxis("y", "porcentual");
     var s = myChart.addSeries(["faixa","distrito"], dimple.plot.bar);
+    window.dados_filtrados = data;
 
     myChart.draw();
     window.grafico = myChart
@@ -119,12 +188,58 @@ function desenha_grafico(data) {
 function atualiza_grafico(data,distrito,tipo) {
     ordem_x = ['Menor que R$ 100 mil','Entre R$ 100 mil e R$ 200 mil','Entre R$ 200 mil e R$ 300 mil','Entre R$ 300 mil e R$ 500 mil','Entre R$ 500 mil e R$ 1 milhão','Mais que R$ 1 milhão']
     var myChart = window.grafico
-    data = dimple.filterData(data, "distrito", ["total",distrito]);
+    data = dimple.filterData(data, "distrito", ["TOTAL",distrito]);
     data = dimple.filterData(data, "tipo", tipo);
     data = dimple.filterData(data, "faixa", ordem_x);
-    console.log(data)
+    window.dados_filtrados = data;
     myChart.data = data;
     myChart.draw();
+}
+
+function atualiza_textos(valor) {
+    var tipo_temp = traduz_tipo(tipo_select)
+    var tipo = tipo_select.replace(" ","_")
+    var faixa = acha_faixa(valor)
+    var distrito = traduz_distrito(distrito_selec)
+
+    if (faixa == "100000") {
+        var num_temp = 0
+        for (var faixa_temp in dados[distrito_selec][tipo]) {
+            if ((faixa_temp != faixa) && (faixa_temp != 'TOTAL')) {
+                num_temp += dados[distrito_selec][tipo][faixa_temp]
+            }
+        }
+        var texto = 'Há pelo menos '+num_temp +' '+tipo_temp+' mais caros que o seu em '
+
+    } else if (faixa == "10000000000") {
+        var num_temp = 0
+        for (var faixa_temp in dados[distrito_selec][tipo]) {
+            if ((faixa_temp != faixa) && (faixa_temp != 'TOTAL')) {
+                num_temp += dados[distrito_selec][tipo][faixa_temp]
+            }
+        }
+        var texto = 'Há pelo menos '+num_temp +' '+tipo_temp+' mais baratos que o seu em '
+
+    } else {
+        var baratos_temp = 0
+        var caros_temp = 0
+        for (var faixa_temp in dados[distrito_selec][tipo]) {
+            if (faixa_temp < faixa) {
+                baratos_temp += dados[distrito_selec][tipo][faixa_temp]
+            } else if (faixa_temp > faixa) {
+                caros_temp += dados[distrito_selec][tipo][faixa_temp]
+            }
+        }
+        var texto = 'Há pelo menos '+baratos_temp +' '+tipo_temp+' mais baratos que o seu e '+ caros_temp +' mais caros em '
+    }
+
+    var dados_temp = dimple.filterData(dados_filtrados,"faixa",conserta_faixa(faixa))
+    dados_temp = dimple.filterData(dados_temp,"distrito",distrito_selec)
+    var perc = dados_temp[0]["porcentual"]
+
+    texto += distrito +". Na faixa de preços referente a esse imóvel, " + conserta_faixa(faixa).toLowerCase().replace("r$","R$") +", estão cerca de " +perc+"% do total deste tipo de imóvel nessa região."
+
+    $("#texto").text(texto)
 }
 
 iniciar()
